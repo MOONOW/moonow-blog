9dc79810e63d59ec040b64310760315b8d78f46c
MOONOW <conatusrock@gmail.com>
2020-08-18 01:03:36 +0900

    test2

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     // edit below
-    title: `Gatsby Starter Personal Blog`,
-    author: `Gatsby`,
+    title: `MOONOW Blog`,
+    author: `MOONOW`,
     description: `A starter personal blog with styled components, dark mode, and Netlify CMS.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
